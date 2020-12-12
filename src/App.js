b74012698a824b7468e920804b848bd10d6dbc82
Viva <happybird00@naver.com>
2020-12-12 21:10:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Viva from "./Viva";
-
+import React from "react";
+function Viva({ name }) {
+  return <h3>내 이름은 {name}</h3>;
+}
 function App() {
   return (
     <div className="App">
       <h3>hello</h3>
-      <Viva></Viva>
+      <Viva name="동현" />
+      <Viva name="Viva" />
+      <Viva name="동욱" />
+      <Viva name="동현" />
     </div>
   );
 }
--- src/Viva.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Viva() {
-  return <h3>데스트 중입니다.</h3>;
-}
-
-export default Viva;
