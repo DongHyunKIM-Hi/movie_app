f59d2ed89efdb87453697e9e635992a48011089a
Viva <happybird00@naver.com>
2020-12-14 20:11:50 +0900

    react array map으로 뿌리기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 function Viva({ name }) {
   return <h3>내 이름은 {name}</h3>;
 }
+const nameList = [
+  {
+    name: "Viva",
+  },
+  {
+    name: "donghyun",
+  },
+  {
+    name: "suu",
+  },
+];
 function App() {
   return (
     <div className="App">
-      <h3>hello</h3>
-      <Viva name="동현" />
-      <Viva name="Viva" />
-      <Viva name="동욱" />
-      <Viva name="안녕하세요 " />
-      <Viva name="오늘은 엄마 생신입니다ㅏ." />
+      {nameList.map((name) => (
+        <Viva name={name.name} />
+      ))}
     </div>
   );
 }
