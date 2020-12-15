62e21862e971117badb17e48e7387699cf355647
Viva <happybird00@naver.com>
2020-12-15 14:48:18 +0900

    state의 관리 setState를 사용하는 방법과 이유

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from "react";
-function Viva({ name }) {
-  return <h3>내 이름은 {name}</h3>;
-}
 
-const nameList = [
-  {
-    id: 1,
-    name: "Viva",
-    image: "123",
-  },
-  {
-    id: 2,
-    name: "donghyun",
-    image: "123",
-  },
-  {
-    id: 3,
-    name: "suu",
-    image: "123",
-  },
-];
-function App() {
-  return (
-    <div className="App">
-      {nameList.map((name) => {
-        return <Viva key={name.id} name={name.name} />;
-      })}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  min = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>i am a class{this.state.count} </h1>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.min}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
