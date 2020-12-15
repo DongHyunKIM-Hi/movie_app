7a66f141343c5e747506f9c97d0a2d3365f68ad0
Viva <happybird00@naver.com>
2020-12-15 13:46:45 +0900

    React map 함수 연습하기

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from "react";
 function Viva({ name }) {
   return <h3>내 이름은 {name}</h3>;
 }
+
 const nameList = [
   {
+    id: 1,
     name: "Viva",
+    image: "123",
   },
   {
+    id: 2,
     name: "donghyun",
+    image: "123",
   },
   {
+    id: 3,
     name: "suu",
+    image: "123",
   },
 ];
 function App() {
   return (
     <div className="App">
-      {nameList.map((name) => (
-        <Viva name={name.name} />
-      ))}
+      {nameList.map((name) => {
+        return <Viva key={name.id} name={name.name} />;
+      })}
     </div>
   );
 }
