7e2c1055564a666c17fa5a0249ad6946db5f9c29
Viva <happybird00@naver.com>
2020-12-16 14:38:57 +0900

    React에 외부 api 연결해서 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    this.setState({ movies: movies, isLoading: false });
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+    const { movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span>"Loading..."</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
